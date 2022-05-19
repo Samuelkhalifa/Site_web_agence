4e7dacc4ca6b4c034fcfd945651d48cb029ebafe
Samuelkhalifa <97511680+Samuelkhalifa@users.noreply.github.com>
2022-05-19 15:20:06 +0200

    Init commit

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+
+const App = () => {
+  return(
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+};
+
+
+
+
+createRoot(document.getElementById('root')).render(<App />); 
