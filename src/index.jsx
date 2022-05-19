f2a1e505509c2a23db8da5ac415be289beed13cc
Samuelkhalifa <97511680+Samuelkhalifa@users.noreply.github.com>
2022-05-19 15:40:40 +0200

    Router + pages

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,16 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 
+import Home from './Home';
+import About from './Home';
+import Works from './Home';
+
+
 const App = () => {
   return(
     <BrowserRouter>
+      <Routes>
+      </Routes>
     </BrowserRouter>
   )
 };
